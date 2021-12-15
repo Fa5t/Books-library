8612269c5b4a6f49e87d2483f4f58160c4090a2f
fast <fadeevstas15@gmail.com>
2021-12-15 14:08:24 +0400

    change all reload page to uploadBooks()

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const uploadBook = document.querySelector('#uploadBook');
-    const writeBook = document.querySelector('#writeBook');
-    const writeForm = document.querySelector('.main__block-add-books__write');
-    const uploadForm = document.querySelector('.main__block-add-books__upload');
+    const uploadBook = document.querySelector('#uploadBook'),
+        writeBook = document.querySelector('#writeBook'),
+        writeForm = document.querySelector('.main__block-add-books__write'),
+        uploadForm = document.querySelector('.main__block-add-books__upload'),
+        dropFav = document.querySelector('.main__block__favourite-books__drag-n-drop-area'),
+        listDrop = document.querySelector('.main__block__list-books__drag-n-drop-area'),
+        listBooks = document.querySelectorAll('.main__block__white-drag');
+    let Books = localStorage.getItem('books') ? JSON.parse(localStorage.getItem('books')) : [];
 
     uploadBook.addEventListener('click', function () {        
             uploadForm.style.display = 'flex';
@@ -13,8 +17,6 @@ document.addEventListener('DOMContentLoaded', function() {
             writeForm.style.display = 'flex';
             uploadForm.style.display = 'none';             
     });
-
-    let Books = localStorage.getItem('books') ? JSON.parse(localStorage.getItem('books')) : [];
 
     document.querySelector('.main__block-add-books__write-btn').addEventListener('click', () => {
         let name = document.querySelector('.main__block-add-books__write-name').value;
@@ -29,7 +31,7 @@ document.addEventListener('DOMContentLoaded', function() {
             };
             Books.push(book);
             localStorage.setItem('books', JSON.stringify(Books));
-            location.reload();
+            uploadBooks()
         } else {
             alert('Введите название и текст книги');
         }
@@ -58,40 +60,26 @@ document.addEventListener('DOMContentLoaded', function() {
             };
             Books.push(book);
             localStorage.setItem('books', JSON.stringify(Books));
-            location.reload();
+            uploadBooks()
         });
     })
 
     function uploadBooks() {
         let favBooks = document.querySelector('.main__block__favourite-books__book');
         let listBooks = document.querySelector('.main__block__list-books__book');
-        const dragNdrop = document.querySelectorAll('.dragNdrop');
         favBooks.innerHTML = '';
-        listBooks.innerHTML = '';    
+        listBooks.innerHTML = '';
         let arr = sortArr();
+        Books = sortArr();
 
         for(let i = 0; i < arr.length; i++) {
             let book = document.createElement('div');
             book.classList.add('book')
             book.classList.add(`book${i}`);
-            book.setAttribute('data-date', arr[i].date);
-            listBooks.appendChild(book);
+            book.setAttribute('data-date', arr[i].date);           
             book.setAttribute('draggable', 'true');
 
-            arr[i].readIt ? book.style.backgroundColor = 'grey' : book.style.backgroundColor = 'white';
-
-            book.innerHTML = `
-                <div class="book__left">
-                            <p class="book__left__text">- ${arr[i].name}</p>
-                        </div>
-                        <div class="book__right">
-                            <button class="btn btn-red btn-red${i}">РЕД.</button>
-                            <button class="btn btn-big btn-big${i}">ПРОЧИТАЛ</button>
-                            <button class="btn btn-med btn-med${i}">ЧИТАТЬ</button>
-                            <button class="btn btn-little btn-little${i}">Х</button>
-                        </div>`;
-
-            if(arr[i].favorite === true) {
+            if(arr[i].favorite) {
                 favBooks.appendChild(book);
                 book.innerHTML = `
                 <div class="book__left">
@@ -103,54 +91,70 @@ document.addEventListener('DOMContentLoaded', function() {
                             <button class="btn btn-med btn-med${i}">ЧИТАТЬ</button>
                             <button class="btn btn-little btn-little${i}">Х</button>
                         </div>`;
-            } 
+            } else {
+                listBooks.appendChild(book);
+                book.innerHTML = `
+                <div class="book__left">
+                            <p class="book__left__text">- ${arr[i].name}</p>
+                        </div>
+                        <div class="book__right">
+                            <button class="btn btn-red btn-red${i}">РЕД.</button>
+                            <button class="btn btn-big btn-big${i}">ПРОЧИТАЛ</button>
+                            <button class="btn btn-med btn-med${i}">ЧИТАТЬ</button>
+                            <button class="btn btn-little btn-little${i}">Х</button>
+                        </div>`;
+            }
+
+            arr[i].readIt ? book.style.backgroundColor = 'grey' : book.style.backgroundColor = 'white';
+
         }
-        document.querySelectorAll('.book__right').forEach( event => {
-            event.addEventListener('click', e => {
-                let date = arr[e.target.className.toString().slice(-1)].date;
+        // document.querySelectorAll('.book__right').forEach( event => {
+        //     event.addEventListener('click', e => {
+        //         let date = arr[e.target.className.toString().slice(-1)].date;
     
-                if(e.target.classList[1] == 'btn-med') {
-                    readIt(date);
-                }
+        //         if(e.target.classList[1] == 'btn-med') {
+        //             readIt(date);
+        //         }
     
-                if(e.target.classList[1] == 'btn-little') {
-                    deleteBook(date);
-                }
+        //         if(e.target.classList[1] == 'btn-little') {
+        //             deleteBook(date);
+        //         }
     
-                if(e.target.classList[1] == 'btn-red') {
-                    editBook(date);
-                }
+        //         if(e.target.classList[1] == 'btn-red') {
+        //             editBook(date);
+        //         }
     
-                if(e.target.classList[1] == 'btn-big') {
-                    changeReadit(date);
-                }
-            })
-        })  
+        //         if(e.target.classList[1] == 'btn-big') {
+        //             changeReadit(date);
+        //         }
+        //     })
+        // })  
+        
     }
 
-    document.querySelectorAll('.book__right').forEach( event => {
-        event.addEventListener('click', e => {
-            let date = arr[e.target.className.toString().slice(-1)].date;
+    document.querySelectorAll('.list-of-books').forEach( item => {
+        item.addEventListener('click', e => {
+            let datee = Books[e.target.className.toString().slice(-1)].date;
 
             if(e.target.classList[1] == 'btn-med') {
-                readIt(date);
+                readIt(datee);
             }
 
             if(e.target.classList[1] == 'btn-little') {
-                deleteBook(date);
+                deleteBook(datee);
             }
 
             if(e.target.classList[1] == 'btn-red') {
-                editBook(date);
-                console.log(e.target);
+                editBook(datee);
+                //console.log(e.target);
             }
 
             if(e.target.classList[1] == 'btn-big') {
-                changeReadit(date);
-                console.log(e.target);
+                changeReadit(datee);
+                //console.log(e.target);
             }
         })
-    })
+    }) 
 
     function readIt(data) {
         let arr = sortArr();
@@ -160,7 +164,7 @@ document.addEventListener('DOMContentLoaded', function() {
         let readBook = document.createElement('div');
         readBook.classList.add('main-right__read__book');
         readText.appendChild(readBook);
-        console.log(read);
+        console.log('readIt');
         readBook.innerHTML = read.text;
 
     }
@@ -171,7 +175,8 @@ document.addEventListener('DOMContentLoaded', function() {
         let num = arr.indexOf(deleteArr);
         arr.splice(num, 1);
         localStorage.setItem('books', JSON.stringify(arr));
-        location.reload();
+
+        uploadBooks();
     }
 
     function editBook(data) {
@@ -205,31 +210,43 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log(arr[num].readIt);
         arr[num].readIt = !arr[num].readIt
         localStorage.setItem('books', JSON.stringify(arr));
-        location.reload();
+        uploadBooks();
     }
 
     function drag() {
-        const listBooks = document.querySelectorAll('.main__block__list-books__book');
-        const favBooks = document.querySelectorAll('.main__block__favourite-books__book');
-        const books = document.querySelectorAll('.book');
+        const listBooks = document.querySelector('.main__block__list-books__book');
+        const favBooks = document.querySelector('.main__block__favourite-books__book');
         const dropFav = document.querySelector('.main__block__favourite-books__drag-n-drop-area');
         const listDrop = document.querySelector('.main__block__list-books__drag-n-drop-area');
+        let id;
 
-        books.forEach(book => {
-            book.addEventListener('dragstart', e => {
+        listBooks.addEventListener('dragstart', e => {
                 e.target.classList.add('selected');
+                id = e.target.getAttribute('data-date');
+                console.log(id);
+                console.log(listBooks.childNodes[0]);
                 //console.log(e.target);
                 console.log('start');
             })
+
+        listBooks.addEventListener('dragend', e => {
+            e.target.classList.remove('selected');
+            console.log('end');
         })
 
-        books.forEach(book => {
-            book.addEventListener('dragend', e => {
-                e.target.classList.remove('selected');
-                //console.log(e.target);
-                console.log('end');
-            })
+        favBooks.addEventListener('dragstart', e => {
+            e.target.classList.add('selected');
+            //console.log(e.target);
+            console.log('start');
         })
+
+        favBooks.addEventListener('dragend', e => {
+            e.target.classList.remove('selected');
+            //console.log(e.target);
+            console.log('end');
+            uploadBooks();
+        })
+
 
         dropFav.addEventListener('dragover', e => {
             e.preventDefault();
@@ -241,15 +258,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
         dropFav.addEventListener('drop', e => {
             e.preventDefault();
-            books.forEach( book => {
-                if(book.classList[2] === 'selected') { 
+            console.log('drop');
+            listBooks.childNodes.forEach (book => {
+                //console.log(book.getAttribute('data-date'));
+                if( book.classList.contains('selected') ) { 
                     let date = book.getAttribute('data-date');
                     let arr = JSON.parse(localStorage.getItem('books'));
                     let newNum = arr.find(item => item.date == date);
                     let num = arr.indexOf(newNum);
                     arr[num].favorite = true;
+                    console.log('good');
                     localStorage.setItem('books', JSON.stringify(arr));
-                    location.reload();
+                    uploadBooks();
                 } else
                 {
                     console.log('error');
@@ -259,7 +279,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
         listDrop.addEventListener('drop', e => {
             e.preventDefault();
-            books.forEach( book => {
+            console.log('drop');
+            favBooks.childNodes.forEach (book => {
+                //console.log(book.getAttribute('data-date'));
                 if(book.classList[2] === 'selected') { 
                     let date = book.getAttribute('data-date');
                     let arr = JSON.parse(localStorage.getItem('books'));
@@ -267,7 +289,8 @@ document.addEventListener('DOMContentLoaded', function() {
                     let num = arr.indexOf(newNum);
                     arr[num].favorite = false;
                     localStorage.setItem('books', JSON.stringify(arr));
-                    location.reload();
+                    uploadBooks();
+                    console.log('good');
                 } else
                 {
                     console.log('error');
@@ -275,6 +298,7 @@ document.addEventListener('DOMContentLoaded', function() {
             })
         })
     }
+    
 
     function sortArr() {
         let arr = takeArr();
@@ -309,4 +333,4 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     uploadBooks();
     drag();
-});
+})
